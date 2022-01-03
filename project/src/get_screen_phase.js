3cc920b9220e1de6cb46c9f8e5e1ebae5ba39089
junniepat <patrickoigwe@gmail.com>
2022-01-03 13:53:48 +0400

    changes to structure

---
+++ project/src/get_screen_phase.js
@@ -0,0 +1,12 @@
+import React from "react"
+    import './get_screen_phase.css';
+
+
+    export default function Get_screen_phase() {
+        return (
+            <>
+            <div className="fhe-mainFrame"><div className="c4ai8sF"><div className="cnD2V4C"><div className="cnAfl0u"><div className="crmC6zZ"><div style={{ width:100%: '$2' }}><p>Claim Registration</p></div></div></div><div className="cW2OT9l"><div style={{ width:100%: '$2' }}><p>Need to get your winshield fixed?  Schedule an appointment Today!</p></div></div><div className="cysnk3z"><div style={{ width:100%: '$2' }}><p>We’ll need to gather a little information to get started.</p></div></div><input className="cBNW2Qk" /><button className="c5YoWVd"><div className="cKsMitx"><div style={{ width:100%: '$2' }}><p>Get Started</p></div></div></button></div></div><div className="cVqSZSM"><div style={{ width:100%: '$2' }}><p>mobile</p></div></div><div className="cKN7Kux"><div style={{ width:100%: '$2' }}><p>desktop</p></div></div><div className="cq1OnFf"></div></div>
+            </>
+        )
+    }
+    
---
+++ project/src/get_started_mobile.js
@@ -0,0 +1,12 @@
+import React from "react"
+    import './get_started_mobile.css';
+
+
+    export default function Get_started_mobile() {
+        return (
+            <>
+            <div className="fhe-mainFrame"><div className="cIRUdqp"><div className="cAB7wkg"><div className="cv4CQAN"><div className="ca6tzFc"><div style={{ width:100%: '$2' }}><p>Claim Registration</p></div></div></div><div className="cTCTD6n"><div style={{ width:100%: '$2' }}><p>Need to get your winshield fixed?  Schedule an appointment Today!</p></div></div><div className="cGozGBx"><div style={{ width:100%: '$2' }}><p>We’ll need to gather a little information to get started.</p></div></div><input className="ciiKqVP" /><button className="cEWK8YA"><div className="cmvybTk"><div style={{ width:100%: '$2' }}><p>Get Started</p></div></div></button></div></div><div className="cgOimwG"><div style={{ width:100%: '$2' }}><p>mobile</p></div></div><div className="cSF2IE7"><div style={{ width:100%: '$2' }}><p>desktop</p></div></div><div className="cMl6w22"></div></div>
+            </>
+        )
+    }
+    
